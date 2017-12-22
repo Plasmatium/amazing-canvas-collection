21eb0a4fab0db7d1e05c67cf4b1540127fccbfb3
Plasmatium <slv309@163.com>
2017-12-22 16:12:18 +0800

    default canvas change to ribbon, not completed yet

--- src/collection/utils/Canvas.ts
+++ src/collection/utils/Canvas.ts
@@ -1,4 +1,6 @@
-import { showFPS } from "./others";
+import { showFPS, ParticleLike } from "./others";
+
+let {random} = Math
 
 export abstract class Canvas{
   canvas: HTMLCanvasElement
@@ -49,27 +51,47 @@ export abstract class Canvas{
 
 class DefaultCanvas extends Canvas {
   step: number
+  checkpoint: ParticleLike['pos'][]
   constructor (public bgColor: string) {
     super(bgColor)
     this.step = 0.0
+
+    let {windowW, windowH} = this
+    let interval = windowW / 10
+    let point1 = { x: -random()*interval, y: random()*windowH }
+    let point2 = { x: -random()*interval, y: random()*windowH }
+
+    this.checkpoint = [point1, point2]
+
+    for(let i=1; i<=10; i++) {
+      let point = { x: random()*interval*i, y: random()*windowW }
+      this.checkpoint.push(point)
+    }
+
+    let point3 = { x: windowW + random()*interval, y: random()*windowH }
+    let point4 = { x: windowW + random()*interval, y: random()*windowH }
+    this.checkpoint.push(point3)
+    this.checkpoint.push(point4)
+
+    this.render()
   }
   clrscr ({bgColor, canvas, ctx, windowH, windowW, data}: Canvas = this) {
-    // ctx.clearRect(0, 0, windowW(), windowH())
     ctx.fillStyle = bgColor
     ctx.fillRect(0, 0, windowW, windowH)
   }
   renderMain ({canvas, ctx, windowH, windowW, data}: Canvas = this) {
-    let r = Math.floor((Math.sin(this.step) / 2 + 0.5) * 255)
-    let g = Math.floor((Math.sin(this.step + Math.PI*2/3) / 2 + 0.5) * 255)
-    let b = Math.floor((Math.sin(this.step - Math.PI*2/3) / 2 + 0.5) * 255)
-    let color = `rgb(${[r,g,b]})`
-    ctx.font = '48px sans'
-    ctx.strokeStyle = color
-    ctx.strokeText('default canvas', 100, 100)
-  }
-  animate ({canvas, ctx, windowH, windowW, data}: Canvas = this) {
-    this.render()
-    this.step += 0.001
+    for(let i=0; i<10 + 2; i++) {
+      let color = [255, 255, 205, 255].map(c => Math.floor(c))
+      ctx.fillStyle = `rgba(${color})`
+
+      let points = this.checkpoint.slice(i, i + 3)
+      ctx.beginPath()
+      ctx.moveTo(points[0].x, points[0].y)
+      ctx.lineTo(points[1].x, points[1].y)
+      ctx.lineTo(points[2].x, points[2].y)
+      ctx.closePath()
+      ctx.fill()
+    }
   }
 }
 
