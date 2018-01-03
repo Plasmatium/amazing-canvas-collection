a3d609ab79910561b70447eb30b0febbf5ea621a
Plasmatium <slv309@163.com>
2018-01-03 16:56:20 +0800

    add z into vertex array, z stands for index, used later in data texture

--- src/collection/glslTest.ts
+++ src/collection/glslTest.ts
@@ -3,18 +3,21 @@
  * uniform类型变量，需要在gl.useProgram之后再传送值去显卡。
  */
 
-import {GLScene, generateGLProgram, jsArr2gRam, gRam2ShaderAttr, dd} from './utils/WebGL'
+import {GLScene, generateGLProgram, jsArr2gRam, gRam2ShaderAttr, dd, dt} from './utils/WebGL'
 import { colorArray, TransferParam, randn_bm } from './utils/others'
 import { normalize } from 'path';
 
 const vsSrc = `
 uniform vec2 u_resolution;
-attribute vec2 a_position;
-attribute vec4 a_color;
+attribute vec3 a_position;
+// a_position.z is index
+varying float v_index;
 varying vec4 v_color;
 void main() {
-  vec2 clip = a_position/u_resolution*2.0 - 1.0;
-  v_color = a_color;
+  v_index = a_position.z;
+
+  vec2 clip = a_position.xy/u_resolution*2.0 - 1.0;
+  v_color = vec4(v_index, 0.4, 0.8, 1);
   gl_Position = vec4(clip, 0, 1);
 }
 `
@@ -31,27 +34,36 @@ const {random} = Math
 
 class GLColorfulDonut extends GLScene {
   public vertices: number[] = []
+  public dataTexture: number[] = []
   constructor (public bgColor: colorArray = [0, 0, 0, 0], count = 500) {
     super()
     let {windowW, windowH, gl} = this
     this.vertices = dd
+    this.dataTexture = dt
 
     // init
     let program = generateGLProgram(gl, vsSrc, fsSrc)
-    let u_resolutionLoc = gl.getUniformLocation(program, 'u_resolution')
     let positionsBuffer = gl.createBuffer()
+
     let pointerConfig = {
-      size: 2,
+      size: 3,
       type: gl.FLOAT,
       normalize: false,
       stride: 0,
       offset: 0
     }
-    
-    let data = new Float32Array(this.vertices)
-    jsArr2gRam(gl, gl.ARRAY_BUFFER, positionsBuffer, data, gl.STATIC_DRAW)
+    // transfer vertices
+    let vertices = new Float32Array(this.vertices)
+    jsArr2gRam(gl, gl.ARRAY_BUFFER, positionsBuffer, vertices, gl.STATIC_DRAW)
     gRam2ShaderAttr(gl, gl.ARRAY_BUFFER, positionsBuffer, 'a_position', pointerConfig, program)
+
+
+
+    // transfer data texture
+    
     gl.useProgram(program)
+    // transform uniform variable
+    let u_resolutionLoc = gl.getUniformLocation(program, 'u_resolution')
     gl.uniform2f(u_resolutionLoc, windowW, windowH)
   }
   renderMain (timestamp: number) {
@@ -61,7 +73,7 @@ class GLColorfulDonut extends GLScene {
     gl.viewport(0, 0, windowW, windowH)
     gl.clearColor(r, g, b, a)
     gl.clear(gl.COLOR_BUFFER_BIT)
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.vertices.length / 2)
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.vertices.length / 3)
   }
 }
 
--- src/collection/utils/WebGL.ts
+++ src/collection/utils/WebGL.ts
@@ -154,8 +154,9 @@ const circleList = [5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
 .map(r => {
   return genCircle(r)
 })
-// circleList.length === 21
-export function genDonut () {
+
+// param index for donut uniform index
+export function genDonut (index: number) {
   let len = circleList.length
   let outerIdx = floor(random() * (len-1) + 1) // [1, len-1]
   let innerIdx = outerIdx
@@ -165,23 +166,33 @@ export function genDonut () {
   let outerC = circleList[outerIdx]
 
   // debug position for x, y
-  let x = random()*800
-  let y = random()*800
+  let x = random()*150
+  let y = random()*150
 
   // flat zip two circle
   let ret: number[] = []
   for (let i=0; i<innerC.length; i+=2) {
-    ret.push(innerC[i]+x, innerC[i+1]+y, outerC[i]+x, outerC[i+1]+y)
+    ret.push(innerC[i]+x, innerC[i+1]+y, index, outerC[i]+x, outerC[i+1]+y, index)
   }
-  ret.push(ret[0], ret[1], ret[2], ret[3]) // 闭合点
-  ret.push(ret[2], ret[3]) // 结尾化点
-  ret.unshift(ret[0], ret[1]) //开头的退化点，不能放前面，会改变index值
+  ret.push(ret[0], ret[1], index, ret[3], ret[4], index) // 闭合点
+  ret.push(ret[3], ret[4], index) // 结尾化点
+  ret.unshift(ret[0], ret[1], index) //开头的退化点，不能放前面，会改变index值
   return ret
 }
 
+let count = 2
 const _dd: number[] = []
-for(let i=0; i<20000; i++) {
-  _dd.push(...genDonut())
+for(let i=0; i<count; i++) {
+  _dd.push(...genDonut(i))
+}
+
+const _dt: number[] = [] //data texure
+for(let i=0; i<count; i++) {
+  let color = [random(), random(), random(), 1]
+  // data: [posX, posY, volX, volY, accX, accY, reserve, reserve]
+  let data = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.0, 0.0]
+  _dt.push(...color, ...data)
 }
 
 export const dd = _dd
+export const dt = _dt
